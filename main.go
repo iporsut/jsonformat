452b1c9248a0813d8fca0d6c024e3dbeb3cfa80b
Weerasak Chongnguluam <singpor@gmail.com>
2015-03-07 18:36:34 +0700

    Extract function use 5 LOC per function rule

--- main.go
+++ main.go
@@ -9,22 +9,38 @@ import (
 )
 
 func main() {
+	defer func() {
+		logError(recover())
+	}()
+	fmt.Printf("%s\n", format(unmarshal(readInput())))
+}
+
+func readInput() (buf []byte) {
 	buf, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		log.Fatal(err)
-		return
+		panic(err)
 	}
+	return
+}
 
-	var inJson map[string]interface{} = make(map[string]interface{})
-	err = json.Unmarshal(buf, &inJson)
+func unmarshal(buf []byte) (data map[string]interface{}) {
+	data = make(map[string]interface{})
+	if err := json.Unmarshal(buf, &data); err != nil {
+		panic(err)
+	}
+	return
+}
+
+func format(data map[string]interface{}) (out string) {
+	buf, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	return string(buf)
+}
+
+func logError(err interface{}) {
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
-	outJson, err := json.MarshalIndent(inJson, "", "    ")
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	fmt.Printf("%s\n", outJson)
 }
